b9f8b5aa3081ea72a687d27a983f0d8c304b0d6b
Maria Plotnikova <plotnikova.ma03@yandex.ru>
2020-09-02 15:42:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int costoftheticketinkopecks = 1500000;
+        int miles =  costoftheticketinkopecks/  2000;
+        System.out.println("При покупке билета стоимостью " + costoftheticketinkopecks / 100 + " руб., Ваш бонус составит " + miles + " бонусных миль.");
+
+    }
+}
