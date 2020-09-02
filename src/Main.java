faff9dd31ac0c7d43d4350a51e28207e263a02c7
Maria Plotnikova <plotnikova.ma03@yandex.ru>
2020-09-02 15:48:45 +0300

    Coefficient

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         int costoftheticketinkopecks = 1500000;
-        int miles =  costoftheticketinkopecks/  2000;
+        int coefficient = 2000;
+        int miles =  costoftheticketinkopecks / coefficient;
         System.out.println("При покупке билета стоимостью " + costoftheticketinkopecks / 100 + " руб., Ваш бонус составит " + miles + " бонусных миль.");
 
     }
