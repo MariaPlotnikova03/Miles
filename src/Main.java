c22338339a5c62076e827a20697d3fabd442fcab
Maria Plotnikova <plotnikova.ma03@yandex.ru>
2020-09-02 17:16:05 +0300

    Correction

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int costoftheticketinkopecks = 1500000;
-        int coefficient = 2000;
-        int miles =  costoftheticketinkopecks / coefficient;
-        System.out.println("При покупке билета стоимостью " + costoftheticketinkopecks / 100 + " руб., Ваш бонус составит " + miles + " бонусных миль.");
+        int costOfTheTicketInKopecks = 15_000_00;
+        int coefficient = 20_00;
+        int miles =  costOfTheTicketInKopecks / coefficient;
+        System.out.println("При покупке билета стоимостью " + costOfTheTicketInKopecks / 100 + " руб., Ваш бонус составит " + miles + " бонусных миль.");
 
     }
 }
